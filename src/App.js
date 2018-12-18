e4e959defa19a536dd88a0547291eae0705e1b58
符建锋 <gzfujianfeng@corp.netease.com>
2018-12-18 16:15:26 +0800

    add build webpack config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
+import { Flex, WhiteSpace } from 'antd-mobile';
 
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <Button>Start</Button>
+            <div className="flex-container">
+                <div className="sub-title">Title</div>
+                <Flex>
+                    <Flex.Item>1</Flex.Item>
+                    <Flex.Item>2</Flex.Item>
+                </Flex>
+                <WhiteSpace size="lg"/>
+                <Flex>
+                    <Flex.Item>3</Flex.Item>
+                    <Flex.Item>4</Flex.Item>
+                    <Flex.Item>5</Flex.Item>
+                </Flex>
+                <WhiteSpace size="lg"/>
+                <Flex>
+                    <Flex.Item>6</Flex.Item>
+                    <Flex.Item>7</Flex.Item>
+                </Flex>
+            </div>
         );
     }
 }
